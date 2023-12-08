a8284971139b555c4959a729a399264ec70dd399
ffloresr1700@alumno.ipn.mx <141371492+FerchoFR@users.noreply.github.com>
2023-12-07 23:24:33 -0600

    Corrección de Dirección
    Corrección de código de créditos

--- Memory_AR/Assets/GotoCreditos.cs
+++ Memory_AR/Assets/GotoCreditos.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class Go_to_Creditos : MonoBehaviour
+public class GotoCreditos : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -11,13 +10,9 @@ public class Go_to_Creditos : MonoBehaviour
         
     }
 
-    // Update is called once per frame 
+    // Update is called once per frame
     void Update()
     {
         
-    }
-    public void Vea_Creditos()
-    {
-        SceneManager.LoadScene("Creditos");
     }
 }
